1f85d5fc3fb169185190904588e430e797f0dab4
Julian Vos <julianvosje@gmail.com>
2025-03-29 12:47:54 +0100

    Spawn an enemy each beat (for testing)

--- code/world.gd
+++ code/world.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+const Enemy = preload('res://scenes/enemy.tscn')
+
+func spawn_enemy():
+	var enemy = Enemy.instantiate()
+	
+	enemy.h_offset = randf_range(-16, 16)
+	enemy.v_offset = randf_range(-16, 16)
+	
+	$Level/Paths/Path2D.add_child(enemy)
+
 func _on_beat_timer_timeout():
+	spawn_enemy()
+	
 	$Level.update()

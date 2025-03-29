8b62a3df114b547b00986687715beae41ac71c3f
Julian Vos <julianvosje@gmail.com>
2025-03-29 17:40:24 +0100

    Fix enemy Y-sorting

--- code/enemy.gd
+++ code/enemy.gd
@@ -36,7 +36,7 @@ func move():
 	
 	var tween = create_tween().set_parallel().set_trans(Tween.TRANS_SINE)
 	
-	tween.tween_property($Sprite2D, 'position', Vector2.ZERO, 0.5).from(previous_position - position)
+	tween.tween_property($Sprite2D, 'position', $Sprite2D.position, 0.5).from($Sprite2D.position + previous_position - position)
 	tween.set_ease(Tween.EASE_OUT).tween_property($Sprite2D, 'offset:y', -15 / $Sprite2D.scale.y, 0.25)
 	tween.set_ease(Tween.EASE_IN).tween_property($Sprite2D, 'offset:y', 0, 0.25).set_delay(0.25)
 	

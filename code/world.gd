f78cd996a16ea030cbc6d1069a67b0046fe9820f
Julian Vos <julianvosje@gmail.com>
2025-03-29 16:46:50 +0100

    Spawn enemies in waves

--- code/world.gd
+++ code/world.gd
@@ -1,16 +1,48 @@
 extends Node2D
 
-const Enemy = preload('res://scenes/enemy.tscn')
+enum EnemyType { REGULAR, TANKY, FAST }
 
-func spawn_enemy():
+const Enemy = preload('res://scenes/enemy.tscn')
+const BEATS_DURING_WAVE = 10
+const BEATS_BETWEEN_WAVES = 20
+
+var wave = [
+	[0, EnemyType.REGULAR],
+	[0.5, EnemyType.FAST],
+]
+# var current_wave = 0
+var beats_during_this_wave = -5
+var beats_until_spawn = 2.0
+var spawn_progress = -beats_until_spawn / 2
+
+func spawn_enemy(type):
 	var enemy = Enemy.instantiate()
 	
+	enemy.type = type
 	enemy.h_offset = randf_range(-16, 16)
-	enemy.v_offset = randf_range(-16, 16)
+	enemy.v_offset = randf_range(-51.2, 0)
 	
 	$Level/Paths/Path2D.add_child(enemy)
 
 func _on_beat_timer_timeout():
-	spawn_enemy()
+	beats_during_this_wave += 1
+	
+	if beats_during_this_wave == BEATS_DURING_WAVE:
+		beats_during_this_wave = -BEATS_BETWEEN_WAVES
+		spawn_progress = -beats_until_spawn / 2
+	elif beats_during_this_wave >= 0:
+		while spawn_progress + beats_until_spawn <= beats_during_this_wave + 1:
+			spawn_progress += beats_until_spawn
+			
+			var wave_progress_ratio = spawn_progress / BEATS_DURING_WAVE 
+			var enemy_type
+			
+			for phase in wave:
+				if wave_progress_ratio >= phase[0]:
+					enemy_type = phase[1]
+				else:
+					break
+			
+			spawn_enemy(enemy_type)
 	
 	$Level.update()

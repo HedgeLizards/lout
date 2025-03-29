2a05266aedfe7b7b5b9ac86866d017562edb09a1
Julian Vos <julianvosje@gmail.com>
2025-03-29 10:31:57 +0100

    Move Enemy's MoveTimer to World as BeatTimer and make it shoot instruments too

--- code/world.gd
+++ code/world.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _on_beat_timer_timeout():
+	for path in $Paths.get_children():
+		for enemy in path.get_children():
+			enemy.move()
+	
+	for tower in $Towers.get_children():
+		tower.shoot()

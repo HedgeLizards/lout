0035c8ea27abf36ff12fc7c8063e61c6f82f46e3
Julian Vos <julianvosje@gmail.com>
2025-03-30 13:36:37 +0200

    Handle different window sizes

--- code/grid.gd
+++ code/grid.gd
@@ -4,7 +4,7 @@ var tile_size = Vector2(32, 32)
 
 
 func to_grid(pos: Vector2) -> Vector2i:
-	return $Cells.local_to_map(pos)
+	return $Cells.local_to_map(get_viewport().canvas_transform.affine_inverse() * pos)
 
 func to_world(tilepos: Vector2i) -> Vector2:
 	return $Cells.map_to_local(tilepos)
--- code/world.gd
+++ code/world.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
 
+const WORLD_SIZE = Vector2(1152, 648)
+
 var culture: int = 100
+
+
+func _ready():
+	RenderingServer.set_default_clear_color(Color.from_rgba8(49, 119, 204))
+	
+	get_viewport().size_changed.connect(update_camera_zoom)
+	
+	update_camera_zoom()
+
+
+func update_camera_zoom():
+	var viewport_size = get_viewport_rect().size
+	
+	$Camera2D.zoom = Vector2.ONE * min(viewport_size.x / WORLD_SIZE.x, viewport_size.y / WORLD_SIZE.y)
 
 
 func _on_beat_timer_timeout():
